7cd046fc163941283e033c1c66aacdf4ff63901a
NCP08 <navercloudplatform08.gmail.com>
2024-01-25 15:30:10 +0900

    switch문을 ifelse로 변경하는 문제 해답

--- Ch03_Control/src/ch03/control05/IfElseEx.java
+++ Ch03_Control/src/ch03/control05/IfElseEx.java
@@ -14,26 +14,18 @@ public class IfElseEx {
 		Scanner sc = new Scanner(System.in);
 		int sel = sc.nextInt();
 		// switch -> ifelse문으로 전환하세요.
-		switch(sel) {
-		case 1:
-			System.out.println("짜장면이 나왔습니다");
-			break;
-		case 2:
-			System.out.println("짬뽕이 나왔습니다");
-			break;
-		case 3:
-			System.out.println("탕수육이 나왔습니다");
-			break;
-		case 4:
+		if(sel==1)
+			System.out.println("짜장면이 나왔습니다");			
+		else if(sel==2)
+			System.out.println("짬뽕이 나왔습니다");			
+		else if(sel==3)
+			System.out.println("탕수육이 나왔습니다");			
+		else if(sel==4)
 			System.out.println("유산슬이 나왔습니다");
-			break;
-		case 5:
+		else if(sel==5)
 			System.out.println("유린기가 나왔습니다");
-			break;
-		default:		// else의 의미
+		else
 			System.out.println("해당 메뉴 번호가 없습니다");
-			break;
-		}
 		
 		sc.close();
 	}
