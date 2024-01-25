2ddd7b98346a3e459b390965dac46c33bac51497
NCP08 <navercloudplatform08.gmail.com>
2024-01-25 16:58:45 +0900

    while문 4개 예제 해결

--- Ch03_Control/src/ch03/while08/Resolve4.java
+++ Ch03_Control/src/ch03/while08/Resolve4.java
@@ -4,7 +4,24 @@ package ch03.while08;
 //while문내에 조건문을 넣어주면 됨
 //짝수 조건 if(num % 2 == 0)
 public class Resolve4 {
-	public static void main(String[] args) {
-
+	public static void method1() {
+		int num=1;
+		while(num <= 10) {
+			if(num%2==0)
+				System.out.println(num);
+			num++;
+		}		
 	}
+	public static void method2() {
+		int num=2;
+		while(num <= 10) {
+			System.out.println(num);
+			num+=2;
+		}	
+	}
+	public static void main(String[] args) {
+//		method1();
+		method2();
+	}
+
 }
