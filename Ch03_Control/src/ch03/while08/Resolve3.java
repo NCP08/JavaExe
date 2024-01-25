ed59d8dee636143df4e2e93659cda05a441638da
NCP08 <navercloudplatform08.gmail.com>
2024-01-25 16:37:53 +0900

    문제 풀기 위한 Resolve1 ~ Resolve4 클래스 4개 생성

--- Ch03_Control/src/ch03/while08/Resolve3.java
+++ Ch03_Control/src/ch03/while08/Resolve3.java
@@ -1,5 +1,8 @@
 package ch03.while08;
 
+//3. while문을 이용해서 1부터 10까지 합을 출력하세요
 public class Resolve3 {
+	public static void main(String[] args) {
 
+	}
 }
--- Ch03_Control/src/ch03/while08/Resolve4.java
+++ Ch03_Control/src/ch03/while08/Resolve4.java
@@ -1,5 +1,10 @@
 package ch03.while08;
 
+//4. while문을 이용해서 1부터 10까지 중에 짝수만 출력하세요
+//while문내에 조건문을 넣어주면 됨
+//짝수 조건 if(num % 2 == 0)
 public class Resolve4 {
+	public static void main(String[] args) {
 
+	}
 }
