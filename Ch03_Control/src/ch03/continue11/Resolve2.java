597012f8224f867407a02b0987bd2fe8151a185e
NCP08 <navercloudplatform08.gmail.com>
2024-01-26 11:34:00 +0900

    교재 138~139 page 3, 6, 7번 문제

---
+++ Ch03_Control/src/ch03/continue11/Resolve2.java
@@ -0,0 +1,11 @@
+package ch03.continue11;
+
+//for문을 이용해서 아래 출력
+//*
+//**
+//***
+//****
+//*****
+public class Resolve2 {
+
+}
---
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -0,0 +1,8 @@
+package ch03.continue11;
+
+//교재 139page 7번
+//while문과 Scanner의 nextLine()메서드를 이용해서
+//예금, 출금, 조회, 종료 기능 제공
+public class Resolve3 {
+
+}
