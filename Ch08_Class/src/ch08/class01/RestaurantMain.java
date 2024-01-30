312b55639d730f34f31d862b4aeb84eb305a2b41
NCP08 <navercloudplatform08.gmail.com>
2024-01-30 14:28:17 +0900

    RestaurantMain.java Restaurant클래스를 사용하는 main함수 작성

--- Ch08_Class/src/ch08/class01/RestaurantMain.java
+++ Ch08_Class/src/ch08/class01/RestaurantMain.java
@@ -2,6 +2,10 @@ package ch08.class01;
 
 public class RestaurantMain {
 	public static void main(String[] args) {
-		
+		Restaurant rest = new Restaurant();
+		rest.viewMenu();
+		rest.selectFood();
+		rest.deliveryFood();
+		rest.keyboardEnd();
 	}
 }
