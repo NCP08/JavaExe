5a72ea90633ff7a432d4cc8d2f1c2fa2ec2db26a
NCP08 <navercloudplatform08.gmail.com>
2024-01-31 12:45:46 +0900

    this를 설명하기 위해 불필요한 것 제거

--- Ch08_Class/src/ch08/class11/WatchTime.java
+++ Ch08_Class/src/ch08/class11/WatchTime.java
@@ -3,38 +3,26 @@ package ch08.class11;
 
 public class WatchTime {
 	private int hour, min, sec;
-	private String strHour, strMin, strSec;
-	private boolean isNum = true;
 	
 	public WatchTime(int hour, int min, int sec) {
+		System.out.println("this:" + this);
 		this.hour = hour;
 		this.min = min;
 		this.sec = sec;
-		this.isNum = true;
 	}	
-	public WatchTime(String strHour, String strMin, String strSec) {
-		this.strHour = strHour;
-		this.strMin = strMin;
-		this.strSec = strSec;
-		this.isNum = false;
-	}
 	public void viewTime() {
-		if(isNum)
-			System.out.printf("[%02d:%02d:%02d]\n", hour, min, sec);
-		else
-			System.out.printf("- %s %s %s -\n", strHour, strMin, strSec);
+		System.out.printf("[%02d:%02d:%02d]\n", hour, min, sec);
 	}
 	
 	public static void main(String[] args) {
 		WatchTime wtNum = new WatchTime(12, 13, 33);
 		System.out.println("wtNum:" + wtNum);
 		System.out.printf("wtNum:%x\n", wtNum.hashCode());
-		wtNum.viewTime();
-		
-		WatchTime wtStr = new WatchTime("열두시", "이십분", "오십초");
-		System.out.println("wtStr:" + wtStr);
-		System.out.printf("wtStr:%x\n", wtStr.hashCode());
-		wtStr.viewTime();
+
+		WatchTime wtNum1 = new WatchTime(13, 23, 53);
+		System.out.println("wtNum1:" + wtNum1);
+		System.out.printf("wtNum1:%x\n", wtNum1.hashCode());
+
 	}
 }
 
