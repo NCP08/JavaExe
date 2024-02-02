12810cad76cabd7dbd52c535f7f4bcddc9a61ed2
NCP08 <navercloudplatform08.gmail.com>
2024-02-02 15:17:07 +0900

    CarTest.java 구현

--- Ch09_Extends/src/ch09/interface13/order01/CarTester.java
+++ Ch09_Extends/src/ch09/interface13/order01/CarTester.java
@@ -15,13 +15,41 @@ public class CarTester {
 	public void setCar(ICar car) {
 		this.car = car;
 	}
-	public void onOffTest() {
+	public int onOffTest() {
 		for(int i=0;i<TEST_NUM;i++) {
 			car.turnOn();
 			car.turnOff();			
 		}
+		return (int)Math.random()*100;	//0이상~99이하
 	}
-	
+	public int speedTest() {
+		for(int i=0;i<TEST_NUM;i++) {
+			car.accel();
+			car.accel();
+			car.accel();
+		}
+		return (int)Math.random()*50 + 50;	//50이상~99이하
+	}
+	public int footBreakTest() {
+		for(int i=0;i<TEST_NUM;i++) {
+			car.accel();
+			car.footBreak();
+			car.footBreak();
+			car.footBreak();
+		}
+		return (int)Math.random()*50 + 50;	//50이상~99이하
+	}
+	public int driveTest() {
+		for(int i=0;i<TEST_NUM;i++) {
+			car.turnOn();
+			car.accel();
+			car.accel();
+			car.footBreak();
+			car.footBreak();
+			car.turnOff();
+		}
+		return (int)Math.random()*50 + 50;	//50이상~99이하
+	}
 }
 
 
