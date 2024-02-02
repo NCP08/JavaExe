801f71b7ca4681b9d0bc721b83bd2f7473ef6b57
NCP08 <navercloudplatform08.gmail.com>
2024-02-02 11:06:12 +0900

    임시직 사원과 일용직 사원 메서드 구현

--- Ch09_Extends/src/ch09/abstract10/EmployeeManager.java
+++ Ch09_Extends/src/ch09/abstract10/EmployeeManager.java
@@ -6,7 +6,7 @@ public class EmployeeManager {
 	private final int EMP_NUM = 100;	// 100명 사원이 최대
 	// Employee의 자식객체들을 저장
 	private Employee[] empArr = new Employee[EMP_NUM];
-	private int numOfEmp = 0;	// 저장된 사원객체 수
+	private int numOfEmp = 0;	// 저장된 사원객체 수, 다음 사원이 저장될 index
 	private Scanner sc = new Scanner(System.in);
 	
 	private int viewMenu() {
@@ -34,22 +34,55 @@ public class EmployeeManager {
 		return emp;
 	}
 	private TempEmployee createTempEmployee() {
-		return null;
+		System.out.print("사번 >> ");
+		String empno = sc.next();
+		System.out.print("이름 >> ");
+		String name = sc.next();
+		System.out.print("연봉 >> ");
+		int yearSalary = sc.nextInt();
+		System.out.print("계약기간 >> ");
+		int hireYear = sc.nextInt();
+		TempEmployee emp = 
+				new TempEmployee(empno, name, yearSalary, hireYear);
+		return emp;
 	}
 	private PartTimeEmployee createPartTimeEmployee() {
-		return null;
+		System.out.print("사번 >> ");
+		String empno = sc.next();
+		System.out.print("이름 >> ");
+		String name = sc.next();
+		System.out.print("일당 >> ");
+		int dailyPay = sc.nextInt();
+		System.out.print("일한 일수 >> ");
+		int workDay = sc.nextInt();
+		PartTimeEmployee emp = 
+				new PartTimeEmployee(empno, name, dailyPay, workDay);
+		return emp;
 	}
 	private boolean saveEmployee(Employee emp) {
-		return true;
+		boolean isSave = true;
+		
+		if(this.numOfEmp < EMP_NUM) {
+			this.empArr[this.numOfEmp] = emp;
+			this.numOfEmp++;
+			isSave = true;
+		}else {
+			isSave = false;
+		}
+		
+		return isSave;
 	}
 	private void viewAllEmployeeInfo() {
-		
+		for(int i=0;i<this.numOfEmp;i++) {
+			this.empArr[i].showEmployeeInfo();
+		}
 	}
 	public void run() {
 		boolean isRun = true;
 		while(isRun) {
 			int selNum = viewMenu();
 			Employee emp = null;
+			
 			switch(selNum) {
 			case EmpMenu.REG_EMP:
 				emp = createRegularEmployee();
