e3af38abff58a59f244ef83d26064f6d712eb36f
NCP08 <navercloudplatform08.gmail.com>
2024-02-05 15:31:58 +0900

    GeneratorThreeNum.java의 getRandomNum() 메서드 구현

--- Ch09_Extends/src/ch09/resolve14/question03/BaseBallMenu.java
+++ Ch09_Extends/src/ch09/resolve14/question03/BaseBallMenu.java
@@ -1,5 +1,6 @@
 package ch09.resolve14.question03;
 
+//메뉴를 담당하는 역할
 public class BaseBallMenu {
 
 }
--- Ch09_Extends/src/ch09/resolve14/question03/DecisionBall.java
+++ Ch09_Extends/src/ch09/resolve14/question03/DecisionBall.java
@@ -1,5 +1,6 @@
 package ch09.resolve14.question03;
 
+//스트라이크, 볼을 판단하는 역할
 public class DecisionBall {
 
 }
