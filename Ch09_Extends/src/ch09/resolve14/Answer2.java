d2241a9b860d12d362bc780d15b949375c599a15
NCP08 <navercloudplatform08.gmail.com>
2024-02-05 14:44:34 +0900

    ch09.resolve13.question02 문제 해결 중...

--- Ch09_Extends/src/ch09/resolve14/Answer2.java
+++ Ch09_Extends/src/ch09/resolve14/Answer2.java
@@ -2,6 +2,12 @@ package ch09.resolve14;
 
 import java.util.Scanner;
 
+import ch09.interface13.factory01.FordCar;
+import ch09.interface13.factory01.HyundaiCar;
+import ch09.interface13.factory01.ToyotaCar;
+import ch09.interface13.order01.CarTester;
+import ch09.resolve14.question02.ITablet;
+
 public class Answer2 implements IQuestionAnswer {
 
 	@Override
@@ -22,8 +28,49 @@ public class Answer2 implements IQuestionAnswer {
 	@Override
 	public void answer(Scanner sc) {
 		sc.nextLine();		// Enter치기 전까지 멈춰 있음
-		System.out.println("2번 문제 실행~");
+
+//		CarTester carTester = new CarTester();
+//		ICar[] carArr = new ICar[] {
+//				new HyundaiCar(),
+//				new ToyotaCar(),
+//				new FordCar()
+//		};
+//		int[] score = new int[carArr.length];
+//		
+//		for(int i=0;i<carArr.length;i++) {
+//			/* DI(Dependency Injection) : 의존성 주입
+//			 * IoC(Inversion of Control) : 역제어
+//			 * */
+//			carTester.setCar(carArr[i]);
+//			
+//			score[i] += carTester.onOffTest();
+//			System.out.printf("---OnOff 테스트 점수는 %d입니다\n", score[i]);
+//			score[i] += carTester.speedTest();
+//			System.out.printf("---Speed 테스트 점수는 %d입니다\n", score[i]);
+//			score[i] += carTester.footBreakTest();
+//			System.out.printf("---Break 테스트 점수는 %d입니다\n", score[i]);
+//			score[i] += carTester.driveTest();
+//			
+//			System.out.printf("===>전체 테스트 점수는 %d입니다\n", score[i]);
+//			System.out.println("---------------------------------------");
+//		}
+//		
+//		// 가장 큰 점수를 받은 값과 인덱스 찾기
+//		int max = score[0];
+//		int maxIdx = 0;
+//		for(int i=1;i<score.length;i++) {
+//			if(max < score[i]) {
+//				max = score[i];
+//				maxIdx = i;
+//			}
+//		}
+//		
+//		System.out.printf("가장 높은 점수를 받은 차는 %d번째 차이고 점수는 %d입니다\n",
+//				maxIdx+1, max);
+//		System.out.println(carArr[maxIdx].getClass().getName());
+		
 		sc.nextLine();		// Enter치기 전까지 멈춰 있음
+
 	}
 
 	@Override
