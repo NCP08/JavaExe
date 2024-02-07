c022ce7cbdfbf3bd480f13bb1f6a49370c265b32
NCP08 <navercloudplatform08.gmail.com>
2024-02-07 15:34:09 +0900

    주석 설명 추가

--- Ch11_String/src/ch11/string02/ch12/sec05/StringTokenizerExample.java
+++ Ch11_String/src/ch11/string02/ch12/sec05/StringTokenizerExample.java
@@ -4,12 +4,14 @@ import java.util.StringTokenizer;
 
 public class StringTokenizerExample {
 	public static void main(String[] args) {
+		// 문자열을 구분짓는 1번째 방법
 		String data1 = "홍길동&이수홍,박연수";
 		String[] arr = data1.split("&|,");
 		for(String token : arr)
 			System.out.println(token);
 		System.out.println();
 		
+		//문자열을 구분짓는 2번째 방법
 		String data2 = "홍길동/이수홍/박연수";
 		StringTokenizer st = new StringTokenizer(data2, "/");
 		while(st.hasMoreTokens()) {
